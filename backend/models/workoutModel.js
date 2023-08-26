e7527b1ae75a16e436e48332a006c0e98684f6af
chandiosamad07 <absamadchandio@gmail.com>
2023-08-26 12:14:42 +0500

    Schema

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Invalid email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+// Other methods (signup and login) can remain the same as in your original schema
+
+module.exports = mongoose.model("Userdata", userSchema);
